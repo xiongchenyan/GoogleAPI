d2167a204e258f7441832f3bf5172f8ad35c1801
chenyanxiong <xiongchenyan@gmail.com>
2015-09-22 19:58:24 -0400

    initial fb dump reduce

---
+++ FreebaseDump/FbDumpReduce.py
@@ -0,0 +1,18 @@
+'''
+Created on Sep 15, 2015 5:27:25 PM
+@author: cx
+
+what I do:
+    reduce the size of fb dump
+        1, discard http://rdf.freebase.com/ns/ in each line
+what's my input:
+    fb dump
+what's my output:
+    fb dump
+
+
+Not very safe to do this now... wait...
+'''
+
+
+
